a49f780ca9e02832eae63dd4b0830d7d4a721112
joshuadhayes <joshuadhayes@ufl.edu>
2017-09-05 23:05:29 -0400

    finished assignment

--- listingController.js
+++ listingController.js
@@ -7,8 +7,21 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.addListing = function() {
+      if ($scope.newCode !== "" && $scope.newName !== "" && $scope.newLat !== "" && $scope.newLong !== "" && $scope.newAddress !== ""){
+        $scope.listings.push({"code": $scope.newCode, "name": $scope.newName, "coordinates": {"latitude": $scope.newLat, "longitude": $scope.newLong}, "address": $scope.newAddress});
+        $scope.newCode = "";
+        $scope.newName = "";
+        $scope.newLat = "";
+        $scope.newLong = "";
+        $scope.newAddress = "";
+      }
+    };
+    $scope.deleteListing = function(index) {
+      $scope.listings.splice(index, 1);
+    };
+    $scope.showDetails = function(index) {
+      $scope.detailedInfo = $scope.listings[index];
+    };
   }
 ]);
