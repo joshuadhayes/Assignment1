57c32c4906119a597ebbaee8b2d4097845dc7ec7
joshuadhayes <joshuadhayes@ufl.edu>
2017-09-06 19:01:04 -0400

    finished assignment

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -9,13 +9,23 @@ var fs = require('fs'),
     Listing = require('./ListingSchema.js'), 
     config = require('./config');
 
+
 /* Connect to your database */
+
+mongoose.connect(config.db.uri);
 
 /* 
   Instantiate a mongoose model for each listing object in the JSON file, 
   and then save it to your Mongo database 
  */
 
+var listingsData = JSON.parse(fs.readFileSync('listings.json', 'utf8'));
+
+Listing.collection.insertMany(listingsData.entries, function(err,r) {
+       if (err) throw err;
+       console.log(r.insertedCount);
+       mongoose.connection.close();
+});
 
 /* 
   Once you've written + run the script, check out your MongoLab database to ensure that 
--- queries.js
+++ queries.js
@@ -1,3 +1,9 @@
+var fs = require('fs'),
+    mongoose = require('mongoose'), 
+    Schema = mongoose.Schema, 
+    Listings = require('./ListingSchema.js'), 
+    config = require('./config');
+
 /* Fill out these functions using Mongoose queries*/
 
 var findLibraryWest = function() {
@@ -5,25 +11,54 @@ var findLibraryWest = function() {
     Find the document that contains data corresponding to Library West,
     then log it to the console. 
    */
+   Listings.find({name: 'Library West'}, function(err, libWest) {
+    if (err) throw err;
+    console.log("Library West Entry is:")
+    console.log(libWest);
+   });
+
 };
+
 var removeCable = function() {
   /*
     Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed 
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
     and remove this listing from your database and log the document to the console. 
    */
+
+  Listings.findOneAndRemove({ code: 'CABL' }, function(err) {
+    if (err) throw err;
+
+    console.log('Cable deleted!');
+  });
 };
+
 var updatePhelpsLab = function() {
   /*
     Phelps Laboratory's address is incorrect. Find the listing, update it, and then 
     log the updated document to the console. 
    */
+
+   Listings.findOneAndUpdate({name: 'Phelps Laboratory'}, {address: "123 boogie woogie lane"}, {returnNewDocument : true}, function(err, lab) {
+    if (err) throw err;
+    console.log("Updated Phelps Lab:")
+    console.log(lab);
+   });
 };
+
 var retrieveAllListings = function() {
   /* 
     Retrieve all listings in the database, and log them to the console. 
    */
+
+   Listings.find({}, function(err, listings) {
+    if (err) throw err;
+    console.log("Retreived all listings:");
+    console.log(listings)
+   });
 };
+
+mongoose.connect(config.db.uri);
 
 findLibraryWest();
 removeCable();
